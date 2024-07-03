46deb9e7eaf53836638d1fc2abadb1ac40f2c13e
udivak <udivak9@gmail.com>
2024-07-04 02:34:49 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathGame
+{
+    public partial class Form1 : Form
+    {
+        Question[] questions;
+        int totalQuestions, questionCounter, index, score;
+        Random random = new Random();
+
+        public Form1(Question[] questions, int totalQuestions, int index = 0)
+        {
+            InitializeComponent();
+            this.questions = questions;
+            this.totalQuestions = totalQuestions;
+            this.questionCounter = 0;
+            this.index = index;
+            this.score = 0;
+        }
+        public void nextQuestion()
+        {
+            lblQuestion.Text = $"#{this.index+1}.  "+this.questions[this.index].toString();
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            int btnAnswer = int.Parse(((Button)sender).Text);
+            int btnTag = Convert.ToInt32(((Button)sender).Tag);         //for coloring;
+
+            if (checkAnswer(btnAnswer))
+            {
+                this.score++;
+            }
+
+            this.index++;
+
+            if (gameOver())
+            {
+                this.Close();
+                Application.Exit();
+            }
+            else
+            {
+                setButtons();
+                nextQuestion();
+            }
+
+        }
+        private bool gameOver()
+        {
+            if (this.index == 10)
+            {
+                int grade = this.score * 10;
+                MessageBox.Show($"Your score is {this.score}/{this.totalQuestions}" + Environment.NewLine +
+                                $"Your Grade is {grade}!","Game Over");
+                return true;
+            }
+            return false;
+        }
+        private bool checkAnswer(int btnANswer) { return btnANswer == this.questions[this.index].getCorrectAnswer(); }
+
+        internal void setButtons()
+        {
+            Button[] buttons = { button1, button2, button3, button4 };
+            int correctIndex = random.Next(0, 4);
+            buttons[correctIndex].Text = $"{this.questions[index].getCorrectAnswer()}";
+            for (int i=0; i<4; i++)
+            {
+                if (i == correctIndex)
+                    continue;
+                buttons[i].Text = $"{random.Next(1, 21)}";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace MathGame
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Random random = new Random();
+            int totalQuestions = 10;
+            string op = "";
+            Question[] questions = new Question[10];
+            for (int i=0; i<totalQuestions; i++)
+            {
+                int num1 = random.Next(1, 11);
+                int num2 = random.Next(1, 11);
+                int choice = random.Next(1, 3);
+                if (choice == 1)
+                    op = "+";
+                if (choice == 2)
+                    op = "-";
+                questions[i] = new Question(num1, num2, op);
+            }
+            Form1 gameForm =new Form1(questions, totalQuestions);
+            gameForm.nextQuestion();
+            gameForm.setButtons();
+            Application.Run(gameForm);
+        }
+    }
+    public class Question
+    {
+        private int num1, num2, correctAnswer;
+        private string op;
+        public Question(int num1, int num2, string op)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.op = op;
+            if (op == "+")
+                correctAnswer = num1 + num2;
+            else if (op == "-")
+            {
+                this.num1 = Math.Max(num1, num2);
+                this.num2 = Math.Min(num1, num2);
+                this.correctAnswer = this.num1 - this.num2;
+            }
+                
+            else
+                throw new ArgumentException("empty operator");
+        }
+        public int getNum1() { return this.num1; }
+        public int getNum2() { return this.num2; }
+        public int getCorrectAnswer() { return this.correctAnswer; }
+        public string toString() { return $"{this.num1}" +op+ $"{this.num2} = ?"; }
+    }
+}
