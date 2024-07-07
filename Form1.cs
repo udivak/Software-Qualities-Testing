9a0909dce5289b2330070ac52a1abef40be0a83b
udivak <udivak9@gmail.com>
2024-07-07 17:36:56 +0300

    added list of wrong questions and display them with correct answer in game-over MessageBox

--- Form1.cs
+++ Form1.cs
@@ -24,13 +24,37 @@ namespace MathGame
             this.totalQuestions = totalQuestions;
             this.index = index;
             this.score = 0;
-
-            progressBar1.Maximum = totalQuestions;
         }
         public void nextQuestion()
         {
             lblQuestion.Text = $"#{this.index+1}.  "+this.questions[this.index].toString();
         }
+
+        //private void checkAnswerEvent(object sender, EventArgs e)
+        //{
+        //    int btnAnswer = int.Parse(((Button)sender).Text);
+        //    int btnTag = Convert.ToInt32(((Button)sender).Tag);         //for coloring;
+
+        //    if (checkAnswer(btnAnswer))
+        //    {
+        //        this.score++;
+        //    }
+
+        //    this.index++;
+
+        //    if (gameOver())
+        //    {
+        //        this.Close();
+        //        Application.Exit();
+        //    }
+        //    else
+        //    {
+        //        setButtons();
+        //        nextQuestion();
+        //    }
+
+        //}
+
         private async void checkAnswerEvent(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
@@ -46,14 +70,17 @@ namespace MathGame
                 this.wrong_answers.Add(this.questions[index]);
                 clickedButton.BackColor = Color.Red;
             }
+
             // Disable all buttons to prevent multiple clicks
             SetButtonsEnabled(false);
-            // Wait for 1 second
+
+            // Wait for 1.5 seconds
             await Task.Delay(1000);
+
             // Reset button colors and re-enable them
             ResetButtonColors();
             SetButtonsEnabled(true);
-            progressBar1.Value = this.index + 1;
+
             this.index++;
             if (gameOver())
             {
@@ -122,27 +149,12 @@ namespace MathGame
         {
             Button[] buttons = { button1, button2, button3, button4 };
             int correctIndex = random.Next(0, 4);
-            int correctAnswer = this.questions[index].getCorrectAnswer();
-
-            HashSet<int> usedNumbers = new HashSet<int> { correctAnswer };
-
-            for (int i = 0; i < 4; i++)
+            buttons[correctIndex].Text = $"{this.questions[index].getCorrectAnswer()}";
+            for (int i=0; i<4; i++)
             {
                 if (i == correctIndex)
-                {
-                    buttons[i].Text = $"{correctAnswer}";
-                }
-                else
-                {
-                    int wrongAnswer;
-                    do
-                    {
-                        wrongAnswer = random.Next(1, 21);
-                    } while (usedNumbers.Contains(wrongAnswer));
-
-                    usedNumbers.Add(wrongAnswer);
-                    buttons[i].Text = $"{wrongAnswer}";
-                }
+                    continue;
+                buttons[i].Text = $"{random.Next(1, 21)}";
             }
         }
     }
